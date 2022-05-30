826313a6d32c2b5d99db32ea8dbbf1e9c7e7734b
Udosinachi <79016579+Jemienachukwu@users.noreply.github.com>
2022-05-30 12:00:32 +0100

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import FindBots from './findBots'
 
 function App() {
   return (
     <div className="App tc pa3 ">
-      <Routes>
-        <Route path="/" element={<FindBots />} />
-      </Routes>
+      <FindBots />
     </div>
   )
 }
