86f57fa16597f9ed45b957c9e78907a7c3fc698e
Jemienachukwu <jemieofoegbu@gmail.com>
2023-06-10 23:04:03 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css'
-// import { Routes, Route } from 'react-router-dom'
-import FindBots from './findBots'
+import "./App.css";
+import FindBots from "./findBots";
 
 function App() {
   return (
     <div className="App tc pa3 ">
       <FindBots />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/findBots.js
+++ src/findBots.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import CardList from './CardList'
-import Search from './Search'
-import Scroll from './scroll'
-import { robots } from './data'
-import './App.css'
+import React from "react";
+import CardList from "./CardList";
+import Search from "./Search";
+import Scroll from "./scroll";
+import { robots } from "./data";
+// import "./App.css";
 const FindBots = () => {
-  const [searchFeild, setSearchField] = React.useState('')
+  const [searchFeild, setSearchField] = React.useState("");
 
   function onSearchChange(event) {
-    setSearchField(event.target.value)
+    setSearchField(event.target.value);
   }
 
   const except = robots.filter((robots) => {
     return robots.username
       .toLocaleLowerCase()
-      .includes(searchFeild.toLocaleLowerCase())
-  })
+      .includes(searchFeild.toLocaleLowerCase());
+  });
 
   return (
     <div>
-      <h1 className="tc f1">ROBOTS</h1>
+      <h1> ROBOTS</h1>
+
+      <p>generate bot</p>
       <Search SearchChange={onSearchChange} />
       <Scroll>
         <CardList robots={except} />
       </Scroll>
     </div>
-  )
-}
+  );
+};
 
-export default FindBots
+export default FindBots;
